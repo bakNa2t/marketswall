c11f12a9ec203ef1c6803d7e0727096de9bbb4cb
bakNa2t <ok2.lock@gmail.com>
2025-09-02 21:02:03 +0300

    Add Link from next

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 export const Navbar = () => {
   return (
     <nav className="flex justify-between h-20 border-b font-medium bg-white">
-      Navbar
+      <Link
+        href="/"
+        className="flex items-center
+       pl-6"
+      >
+        <span className={cn("text-5xl font-semibold", poppins.className)}>
+          Marketswall
+        </span>
+      </Link>
     </nav>
   );
 };
