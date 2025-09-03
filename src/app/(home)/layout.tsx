65501c2e34a4baf09f2b80c37014bfb808a1c964
bakNa2t <ok2.lock@gmail.com>
2025-09-03 20:35:57 +0300

    Add Navbar

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,16 @@
+import { Navbar } from "./navbar";
+
 interface HomeLayoutProps {
   children: React.ReactNode;
 }
 
 const HomeLayout = ({ children }: HomeLayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default HomeLayout;
