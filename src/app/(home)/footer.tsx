6ae5b95a59854971724dbdd4d4b0f44515d8f533
bakNa2t <ok2.lock@gmail.com>
2025-09-06 07:49:12 +0300

    Create Footer

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-between p-6 border-t font-medium">
+      <div className="flex items-center gap-2">
+        <p>marketswall, Inc.</p>
+      </div>
+    </footer>
+  );
+};
