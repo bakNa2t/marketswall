7b8fbdb61ea318c985e146d524c51b805db09cac
bakNa2t <ok2.lock@gmail.com>
2025-09-06 07:36:50 +0300

    Add shadcn Sheet and ScrollArea for sidebar menu

--- src/app/(home)/navbar-sidebar.tsx
+++ src/app/(home)/navbar-sidebar.tsx
@@ -1,4 +1,12 @@
-import { Sheet, SheetContent } from "@/components/ui/sheet";
+import Link from "next/link";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 
 interface NavbarItemProps {
   href: string;
@@ -18,7 +26,41 @@ export const NavbarSidebar = ({
 }: NavbarSidebarProps) => {
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent>{items}</SheetContent>
+      <SheetContent side="left" className="p-0 transition-0">
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+
+        <ScrollArea className="flex flex-col h-full overflow-y-auto pb-2">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="flex items-center w-full p-4 text-left text-base font-medium hover:bg-black hover:text-white hover:cursor-pointer"
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
+          ))}
+
+          <div className="border-t">
+            <Link
+              href="/sign-in"
+              className="flex items-center w-full p-4 text-left text-base font-medium hover:bg-black hover:text-white hover:cursor-pointer"
+              onClick={() => onOpenChange(false)}
+            >
+              Log in
+            </Link>
+            <Link
+              href="/sign-up"
+              className="flex items-center w-full p-4 text-left text-base font-medium hover:bg-black hover:text-white hover:cursor-pointer"
+              onClick={() => onOpenChange(false)}
+            >
+              Start selling
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
     </Sheet>
   );
 };
