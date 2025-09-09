8550431fc2c41e07878ee831579d3ca29c1df327
bakNa2t <ok2.lock@gmail.com>
2025-09-09 08:16:03 +0300

    Add slug/color/parent/subcategories fields

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,4 +1,4 @@
-import type { CollectionConfig } from "payload";
+import type { CollectionConfig, CollectionSlug } from "payload";
 
 export const Categories: CollectionConfig = {
   slug: "categories",
@@ -7,6 +7,30 @@ export const Categories: CollectionConfig = {
       name: "name",
       type: "text",
       required: true,
+    },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories" as CollectionSlug,
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories" as CollectionSlug,
+      on: "parent",
+      hasMany: true,
     },
   ],
 };
