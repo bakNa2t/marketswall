b845f30497cb2d9dd3a6cf371e52974bc5f79776
bakNa2t <ok2.lock@gmail.com>
2025-09-12 08:03:23 +0300

    Add formattedData to SearchFilters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,9 +1,10 @@
 import configPromise from "@payload-config";
-import { CollectionSlug, getPayload } from "payload";
+import { getPayload } from "payload";
 
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
+import { Category } from "@/payload-types";
 
 interface HomeLayoutProps {
   children: React.ReactNode;
@@ -13,8 +14,9 @@ const HomeLayout = async ({ children }: HomeLayoutProps) => {
   const payload = await getPayload({ config: configPromise });
 
   const data = await payload.find({
-    collection: "categories" as CollectionSlug,
+    collection: "categories",
     depth: 1,
+    pagination: false,
     where: {
       parent: {
         exists: false,
@@ -22,10 +24,18 @@ const HomeLayout = async ({ children }: HomeLayoutProps) => {
     },
   });
 
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      ...(doc as Category),
+      subcategories: undefined,
+    })),
+  }));
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <SearchFilters data={data} />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#f4f4f0]">{children}</div>
       <Footer />
     </div>
