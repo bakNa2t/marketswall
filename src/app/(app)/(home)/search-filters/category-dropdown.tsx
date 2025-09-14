22b6b820f06bc91194177f4e43c969cef0293dd6
bakNa2t <ok2.lock@gmail.com>
2025-09-14 08:41:59 +0300

    Add onMouseEnter/Leave fn to show dropdown list

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useRef, useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import { Category } from "@/payload-types";
 
@@ -14,15 +17,44 @@ export const CategoryDropdown = ({
   isActive,
   isNavigationHovered,
 }: CategoryDropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const onMouseEnter = () => {
+    if (category.subcategories) {
+      setIsOpen(true);
+    }
+  };
+
+  const onMouseLeave = () => setIsOpen(false);
+
   return (
-    <Button
-      variant="elevated"
-      className={cn(
-        "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-        isActive && !isNavigationHovered && "bg-white border-primary"
-      )}
+    <div
+      ref={dropdownRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      className="relative"
     >
-      {category.name}
-    </Button>
+      <div className="relative">
+        <Button
+          variant="elevated"
+          className={cn(
+            "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigationHovered && "bg-white border-primary"
+          )}
+        >
+          {category.name}
+        </Button>
+
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div
+            className={cn(
+              "absolute opacity-0 -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left -1/2 -translate-1/2",
+              isOpen && "opacity-100"
+            )}
+          />
+        )}
+      </div>
+    </div>
   );
 };
