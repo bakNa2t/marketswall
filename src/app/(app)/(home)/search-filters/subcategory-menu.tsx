541fc4196da2e932927c85ac29b4bcbe1b2c6ece
bakNa2t <ok2.lock@gmail.com>
2025-09-15 07:43:34 +0300

    Create SubcategoryMenu

---
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -0,0 +1,38 @@
+import { Category } from "@/payload-types";
+
+interface SubcategoryMenuProps {
+  category: Category;
+  isOpen: boolean;
+  position: { top: number; left: number };
+}
+
+export const SubcategoryMenu = ({
+  category,
+  isOpen,
+  position,
+}: SubcategoryMenuProps) => {
+  if (
+    !isOpen ||
+    !category.subcategories ||
+    category.subcategories.length === 0
+  ) {
+    return null;
+  }
+
+  const backgroundColor = category.color || "#F5F5F5";
+
+  return (
+    <div
+      className="fixed z-100"
+      style={{ top: position.top, left: position.left }}
+    >
+      <div className="w-60 h-3" />
+      <div
+        style={{ backgroundColor }}
+        className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
+      >
+        <p>Subcategory menu</p>
+      </div>
+    </div>
+  );
+};
