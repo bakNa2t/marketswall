5da4e72ac4c759213b13113825f33b8c116254b8
bakNa2t <ok2.lock@gmail.com>
2025-09-17 08:22:45 +0300

    Update category's btn style if it's isOpen true

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -45,7 +45,9 @@ export const CategoryDropdown = ({
           variant="elevated"
           className={cn(
             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-            isActive && !isNavigationHovered && "bg-white border-primary"
+            isActive && !isNavigationHovered && "bg-white border-primary",
+            isOpen &&
+              "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-4-[4px]"
           )}
         >
           {category.name}
@@ -54,7 +56,7 @@ export const CategoryDropdown = ({
         {category.subcategories && category.subcategories.length > 0 && (
           <div
             className={cn(
-              "absolute opacity-0 -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left -1/2 -translate-1/2",
+              "absolute opacity-0 -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-1/2",
               isOpen && "opacity-100"
             )}
           />
