66d1e36efa02baa159c02d689c4950659976cc4d
bakNa2t <ok2.lock@gmail.com>
2025-09-22 07:56:45 +0300

    Add Sheet and ScrollArea to disp cat

--- src/app/(app)/(home)/search-filters/categories-sidebar.tsx
+++ src/app/(app)/(home)/search-filters/categories-sidebar.tsx
@@ -1,16 +1,58 @@
+import { useState } from "react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
 import { CustomCategory } from "../types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ChevronLeftIcon } from "lucide-react";
 
 interface CategoriesSidebarProps {
-  isOpen: boolean;
+  open: boolean;
   onOpenChange: (open: boolean) => void;
   data: CustomCategory[];
 }
 
 export const CategoriesSidebar = ({
-  isOpen,
+  open,
   onOpenChange,
   data,
 }: CategoriesSidebarProps) => {
-  console.log(data, isOpen, onOpenChange);
-  return <div>Categories Sidebar</div>;
+  const [parentCategories, setParentCategories] = useState<
+    CustomCategory[] | null
+  >(null);
+  const [selectedCategory, setSelectedCategory] =
+    useState<CustomCategory | null>(null);
+
+  const currentCategory = parentCategories ?? data ?? [];
+
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent
+        side="left"
+        className="p-0 transition-none"
+        style={{ backgroundColor: "white" }}
+      >
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Categories</SheetTitle>
+        </SheetHeader>
+
+        <ScrollArea className="flex flex-col h-full pb-2 overflow-y-auto">
+          {parentCategories && (
+            <button
+              className="flex items-center w-full p-4 text-base font-medium text-left hover:bg-black hover:text-white hover:cursor-pointer"
+              onClick={() => {}}
+            >
+              <ChevronLeftIcon className="size-4 mr-2" />
+              Back
+            </button>
+          )}
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
 };
