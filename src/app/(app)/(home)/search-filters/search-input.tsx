da0595944ab4bf2de6fd05d6a7fd30993dccf2b1
bakNa2t <ok2.lock@gmail.com>
2025-09-27 13:00:50 +0300

    Add categories ListFilter icon btn for mobile

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,14 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+
+import { CategoriesSidebar } from "./categories-sidebar";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+
+import { CustomCategory } from "../types";
+import { Button } from "@/components/ui/button";
 
 interface SearchInputProps {
   disabled?: boolean;
+  data: CustomCategory[];
 }
 
-export const SearchInput = ({ disabled }: SearchInputProps) => {
+export const SearchInput = ({ disabled, data }: SearchInputProps) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex items-center gap-2 w-full">
       <div className="relative w-full">
+        <CategoriesSidebar
+          data={data}
+          open={isSidebarOpen}
+          onOpenChange={setIsSidebarOpen}
+        />
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
         <Input
           className="pl-8"
@@ -16,6 +32,14 @@ export const SearchInput = ({ disabled }: SearchInputProps) => {
           disabled={disabled}
         />
       </div>
+
+      <Button
+        variant="elevated"
+        className="flex  lg:hidden shrink-0 size-12"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon className="size-4" />
+      </Button>
     </div>
   );
 };
