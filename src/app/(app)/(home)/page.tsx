13ecf04cb94d94039c5063769856edaaaac046aa
bakNa2t <ok2.lock@gmail.com>
2025-09-29 07:34:25 +0300

    Add categoriesRouter and useTRPC to fetch data

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
 
 export default function Home() {
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <h1 className="text-3xl font-bold">Wellcome to Marketswall</h1>
-        <p>Find your own tenant here</p>
-        <Button variant="elevated">Get started</Button>
+        {JSON.stringify(categories.data, null, 2)}
       </main>
     </div>
   );
