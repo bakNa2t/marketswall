5973800a1fe107f047fae7e0343eaa3dd3c00c54
bakNa2t <ok2.lock@gmail.com>
2025-10-05 09:40:16 +0300

    Add register procedure to auth

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -1,6 +1,9 @@
+import z from "zod";
+
 import { headers as getHeaders } from "next/headers";
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { password } from "payload/shared";
 
 export const authRouter = createTRPCRouter({
   session: baseProcedure.query(async ({ ctx }) => {
@@ -10,4 +13,34 @@ export const authRouter = createTRPCRouter({
 
     return session;
   }),
+  register: baseProcedure
+    .input(
+      z.object({
+        email: z.string(),
+        password: z.string(),
+        username: z
+          .string()
+          .min(3, "Username must be at least 3 characters long")
+          .max(63, "Username must be less than 63 characters")
+          .regex(
+            /^[a-z0-9] [a-z0-9-]*[a-z0-9]$/,
+            "Username must start and end with a letter or number, and contain only letters, numbers, and hyphens. It must start with a letter or number"
+          )
+          .refine(
+            (val) => !val.includes("--"),
+            "Username cannot contain consecutive hyphens"
+          )
+          .transform((val) => val.toLowerCase()),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.create({
+        collection: "users",
+        data: {
+          email: input.email,
+          username: input.username,
+          password: input.password,
+        },
+      });
+    }),
 });
