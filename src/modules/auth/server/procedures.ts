ff073196aea6bc37e1cf4b0e7504e332f3c6f57d
bakNa2t <ok2.lock@gmail.com>
2025-10-06 07:39:10 +0300

    Add login procedure to auth

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -1,9 +1,11 @@
 import z from "zod";
 
-import { headers as getHeaders } from "next/headers";
+import { headers as getHeaders, cookies as getCookies } from "next/headers";
 
+import { TRPCError } from "@trpc/server";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { password } from "payload/shared";
+
+import { AUTH_COOKIE } from "../constants";
 
 export const authRouter = createTRPCRouter({
   session: baseProcedure.query(async ({ ctx }) => {
@@ -42,5 +44,38 @@ export const authRouter = createTRPCRouter({
           password: input.password,
         },
       });
+    }),
+  login: baseProcedure
+    .input(
+      z.object({
+        email: z.string(),
+        password: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const data = await ctx.db.login({
+        collection: "users",
+        data: {
+          email: input.email,
+          password: input.password,
+        },
+      });
+
+      if (!data.token) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "Invalid email or password",
+        });
+      }
+
+      const cookies = await getCookies();
+      cookies.set({
+        name: AUTH_COOKIE,
+        value: data.token,
+        httpOnly: true,
+        path: "/",
+      });
+
+      return data;
     }),
 });
