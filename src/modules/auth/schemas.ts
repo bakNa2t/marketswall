0ead1a317021b754c0bd4b7b4c37ec5807cc0431
bakNa2t <ok2.lock@gmail.com>
2025-10-09 07:30:13 +0300

    Create auth schemas

---
+++ src/modules/auth/schemas.ts
@@ -0,0 +1,19 @@
+import z from "zod";
+
+export const registerSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .max(63, "Username must be less than 63 characters")
+    .regex(
+      /^[a-z0-9] [a-z0-9-]*[a-z0-9]$/,
+      "Username must start and end with a letter or number, and contain only letters, numbers, and hyphens. It must start with a letter or number"
+    )
+    .refine(
+      (val) => !val.includes("--"),
+      "Username cannot contain consecutive hyphens"
+    )
+    .transform((val) => val.toLowerCase()),
+});
