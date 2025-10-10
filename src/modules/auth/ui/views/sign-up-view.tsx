9deea8a6d2b31ba205ec746aa971aa44556417ec
bakNa2t <ok2.lock@gmail.com>
2025-10-10 07:50:14 +0300

    Add Form with Link to main page

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,8 +1,61 @@
+"use client";
+
+import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { registerSchema } from "../../schemas";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["700"],
+});
+
 export const SignUpView = () => {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
+
+  const onSubmint = (values: z.infer<typeof registerSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5">
       <div className="bg-[#f4f4f0] h-screen w-full lg:col-span-3 overflow-auto">
-        Form colunm
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmint)}
+            className="flex flex-col gap-8 p-4 lg:p-16"
+          >
+            <div className="flex items-center justify-between mb-8">
+              <Link href="/">
+                <span
+                  className={cn("text-2xl font-semibold", poppins.className)}
+                >
+                  marketswall
+                </span>
+              </Link>
+            </div>
+          </form>
+        </Form>
       </div>
 
       <div
