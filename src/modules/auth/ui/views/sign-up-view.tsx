34834cc42e9fc8eeafb42751a6a00797aeff8d1e
bakNa2t <ok2.lock@gmail.com>
2025-10-11 12:20:30 +0300

    Add FormField with Input comp for username and email

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -6,16 +6,19 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Poppins } from "next/font/google";
 
-import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
+import { cn } from "@/lib/utils";
 import { registerSchema } from "../../schemas";
 
 const poppins = Poppins({
@@ -25,6 +28,7 @@ const poppins = Poppins({
 
 export const SignUpView = () => {
   const form = useForm<z.infer<typeof registerSchema>>({
+    mode: "all",
     resolver: zodResolver(registerSchema),
     defaultValues: {
       email: "",
@@ -36,6 +40,11 @@ export const SignUpView = () => {
   const onSubmint = (values: z.infer<typeof registerSchema>) => {
     console.log(values);
   };
+
+  const username = form.watch("username");
+  const usernameErrors = form.formState.errors.username;
+
+  const showPreview = username && !usernameErrors;
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5">
@@ -53,7 +62,58 @@ export const SignUpView = () => {
                   marketswall
                 </span>
               </Link>
+
+              <Button
+                asChild
+                variant="ghost"
+                size="sm"
+                className="text-base border-none underline"
+              >
+                <Link prefetch href="/sign-in">
+                  Sign in
+                </Link>
+              </Button>
             </div>
+
+            <h1 className="text-4xl font-medium">
+              Join over 1000 creators earning money on Marketswall
+            </h1>
+
+            {/* username input */}
+            <FormField
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+
+                  <FormDescription
+                    className={cn("hidden", showPreview && "block")}
+                  >
+                    Your market will be avialable at&nbsp;
+                    <strong>{username}</strong>
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* email input */}
+            <FormField
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </form>
         </Form>
       </div>
