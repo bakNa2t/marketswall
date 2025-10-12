e481bffbdd918bf765a2b131246d3e7731c23a4f
bakNa2t <ok2.lock@gmail.com>
2025-10-12 19:58:24 +0300

    Add data from trps's auth session

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.auth.session.queryOptions());
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        Welcome to Marketswall
+        {JSON.stringify(data, null, 2)}
       </main>
     </div>
   );
