6b2ccbcd71c7ec08ba6c934c04762bc076c9c7b9
bakNa2t <ok2.lock@gmail.com>
2025-10-15 07:27:59 +0300

    Add login form and trpc mutation

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -1,13 +1,145 @@
+"use client";
+
+import Link from "next/link";
+import { z } from "zod";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Poppins } from "next/font/google";
+import { useMutation } from "@tanstack/react-query";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { loginSchema } from "../../schemas";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["700"],
+});
+
 export const SignInView = () => {
+  const router = useRouter();
+
+  const trpc = useTRPC();
+  const login = useMutation(
+    trpc.auth.login.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess: () => {
+        router.push("/");
+      },
+    })
+  );
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    mode: "all",
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    login.mutate(values);
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5">
       <div className="bg-[#f4f4f0] h-screen w-full lg:col-span-3 overflow-auto">
-        Form colunm
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-8 p-4 lg:p-16"
+          >
+            <div className="flex items-center justify-between mb-8">
+              <Link href="/">
+                <span
+                  className={cn("text-2xl font-semibold", poppins.className)}
+                >
+                  marketswall
+                </span>
+              </Link>
+
+              <Button
+                asChild
+                variant="ghost"
+                size="sm"
+                className="text-base border-none underline"
+              >
+                <Link prefetch href="/sign-up">
+                  Sign up
+                </Link>
+              </Button>
+            </div>
+
+            <h1 className="text-4xl font-medium">
+              Welcome back to Marketswall
+            </h1>
+
+            {/* email input */}
+            <FormField
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* password input */}
+            <FormField
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              size="lg"
+              variant="elevated"
+              className="bg-black text-white hover:bg-pink-400 hover:text-primary"
+              disabled={login.isPending}
+            >
+              Log in
+            </Button>
+          </form>
+        </Form>
       </div>
 
-      <div className="h-screen w-full lg:col-span-2 hidden lg:block">
-        Background colunm
-      </div>
+      <div
+        className="h-screen w-full lg:col-span-2 hidden lg:block"
+        style={{
+          backgroundImage: 'url("/images/auth-bg.jpg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
     </div>
   );
 };
