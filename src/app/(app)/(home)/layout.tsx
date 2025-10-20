6cd570ad3b1ca5a736dfbe1c294c694687882e0e
bakNa2t <ok2.lock@gmail.com>
2025-10-20 07:32:17 +0300

    Move home comps to modules

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -2,9 +2,12 @@ import { Suspense } from "react";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import { Footer } from "./footer";
-import { Navbar } from "./navbar";
-import { SearchFilterSkeleton, SearchFilters } from "./search-filters";
+import { Footer } from "@/modules/home/ui/components/footer";
+import { Navbar } from "@/modules/home/ui/components/navbar";
+import {
+  SearchFilterSkeleton,
+  SearchFilters,
+} from "@/modules/home/ui/components/search-filters";
 
 interface HomeLayoutProps {
   children: React.ReactNode;
