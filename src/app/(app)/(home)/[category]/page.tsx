cbb18c3a8796cb3e13f591d820695ea4c6fc11de
bakNa2t <ok2.lock@gmail.com>
2025-10-21 07:30:12 +0300

    Create dynamic Category page

---
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -0,0 +1,13 @@
+interface CategoryPageProps {
+  params: Promise<{
+    category: string;
+  }>;
+}
+
+const CategoryPage = async ({ params }: CategoryPageProps) => {
+  const { category } = await params;
+
+  return <div>Category is: {category}</div>;
+};
+
+export default CategoryPage;
