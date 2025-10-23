ab96709e6ecc8891cf169bc0aa3ddbeb801c2e2c
bakNa2t <ok2.lock@gmail.com>
2025-10-23 07:22:58 +0300

    Add activeCategoryColor to change bg

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,18 +1,40 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
 
 import { useTRPC } from "@/trpc/client";
+import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
 
 export const SearchFilters = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
 
+  const params = useParams();
+  const categoryParam = params.category as string | undefined;
+  const activeCategory = categoryParam || "all";
+
+  const activeCategoryData = data.find(
+    (category) => category.slug === activeCategory
+  );
+
+  const activeCategoryColor = activeCategoryData?.color || DEFAULT_BG_COLOR;
+  const activeCategoryName = activeCategoryData?.name || null;
+
+  const activeSubcategory = params.subcategory as string | undefined;
+  const activeSubcategoryName =
+    activeCategoryData?.subcategories?.find(
+      (subcategory) => subcategory.slug === activeSubcategory
+    )?.name || null;
+
   return (
-    <div className="flex flex-col gap-4 w-full px-4 lg:px-12 py-8 border-b">
+    <div
+      className="flex flex-col gap-4 w-full px-4 lg:px-12 py-8 border-b"
+      style={{ backgroundColor: activeCategoryColor }}
+    >
       <SearchInput />
 
       <div className="hidden lg:block">
