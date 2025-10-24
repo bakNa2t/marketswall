a1d51009316ebbd27632bb0fe315e91441a46f77
bakNa2t <ok2.lock@gmail.com>
2025-10-24 07:49:20 +0300

    Add Breadcrumb to display categories path

--- src/modules/home/ui/components/search-filters/breadcrumbs-navigation.tsx
+++ src/modules/home/ui/components/search-filters/breadcrumbs-navigation.tsx
@@ -1,3 +1,13 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+
 interface BreadcrumbsNavigationProps {
   activeCategoryName: string | null;
   activeSubcategoryName: string | null;
@@ -9,5 +19,38 @@ export const BreadcrumbsNavigation = ({
   activeSubcategoryName,
   activeCategory,
 }: BreadcrumbsNavigationProps) => {
-  return <div>BreadcrumbsNavigation</div>;
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {activeSubcategoryName ? (
+          <>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                asChild
+                className="text-xl font-medium underline text-primary"
+              >
+                <Link href={`/${activeCategory}`}>{activeCategoryName}</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbSeparator className="text-xl text-primary font-medium">
+              /
+            </BreadcrumbSeparator>
+
+            <BreadcrumbItem>
+              <BreadcrumbPage className="text-xl font-medium">
+                {activeCategoryName}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        ) : (
+          <BreadcrumbItem>
+            <BreadcrumbPage className="text-xl font-medium">
+              {activeCategoryName}
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        )}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
 };
