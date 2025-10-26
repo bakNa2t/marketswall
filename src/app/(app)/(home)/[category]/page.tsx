39068c16d5a1e9e480cb1d5fbef09eebb3887ae1
bakNa2t <ok2.lock@gmail.com>
2025-10-26 08:58:43 +0300

    Wrap ProductList with tanstack's HydrationBoundary

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,13 +1,25 @@
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { ProductList } from "@/modules/products/ui/components/product-list";
+
 interface CategoryPageProps {
   params: Promise<{
     category: string;
   }>;
 }
 
-const CategoryPage = async ({ params }: CategoryPageProps) => {
-  const { category } = await params;
+const CategoryPage = async ({}: CategoryPageProps) => {
+  const queryClient = getQueryClient();
 
-  return <div>Category is: {category}</div>;
+  void queryClient.prefetchQuery(trpc.products.getMany.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductList />
+    </HydrationBoundary>
+  );
 };
 
 export default CategoryPage;
