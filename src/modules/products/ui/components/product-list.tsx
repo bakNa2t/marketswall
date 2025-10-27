30031f023f44255b907ab8a3351d0018212941de
bakNa2t <ok2.lock@gmail.com>
2025-10-27 07:53:20 +0300

    Create ProductListSkeleton and add optional category

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -4,9 +4,19 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
 
-export const ProductList = () => {
+interface ProductListProps {
+  category?: string;
+}
+
+export const ProductList = ({ category }: ProductListProps) => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.products.getMany.queryOptions());
+  const { data } = useSuspenseQuery(
+    trpc.products.getMany.queryOptions({ category })
+  );
 
   return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export const ProductListSkeleton = () => {
+  return <div>Loading ...</div>;
 };
