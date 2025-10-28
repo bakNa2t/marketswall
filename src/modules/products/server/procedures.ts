802ecced6dc4af4c27523937515c6bef6400aa62
bakNa2t <ok2.lock@gmail.com>
2025-10-28 07:28:36 +0300

    Add input method to baseProcedure

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,12 +1,45 @@
+import { string, z } from "zod";
+import type { Where } from "payload";
+
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const productsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async ({ ctx }) => {
-    const data = await ctx.db.find({
-      collection: "products",
-      depth: 1, // populate "category" & "image"
-    });
+  getMany: baseProcedure
+    .input(
+      z.object({
+        category: string().nullable().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const where: Where = {};
 
-    return data;
-  }),
+      if (input.category) {
+        // const categoriesData = await ctx.db.find({
+        //   collection: "categories",
+        //   limit: 1,
+        //   pagination: false,
+        //   where: {
+        //     slug: {
+        //       equals: input.category,
+        //     },
+        //   },
+        // });
+
+        // const category = categoriesData.docs[0];
+
+        // if (category) {
+        where["category.slug"] = {
+          equals: input.category,
+        };
+        // }
+      }
+
+      const data = await ctx.db.find({
+        collection: "products",
+        depth: 1, // populate "category" & "image"
+        where,
+      });
+
+      return data;
+    }),
 });
