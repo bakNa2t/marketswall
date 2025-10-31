f068afbba657a436dfd91cd3b89c8fbb14c14540
bakNa2t <ok2.lock@gmail.com>
2025-10-31 07:31:44 +0300

    Add NuqsAdapter to wrapping

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,8 @@
 import { DM_Sans } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-import { TRPCReactProvider } from "@/trpc/client";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
+import { TRPCReactProvider } from "@/trpc/client";
 import type { Metadata } from "next";
 
 import "./globals.css";
@@ -23,8 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${dmSans.className} antialiased`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-        <Toaster />
+        <NuqsAdapter>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+          <Toaster />
+        </NuqsAdapter>
       </body>
     </html>
   );
