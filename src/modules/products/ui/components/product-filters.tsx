942fa4897f8fc382b732b38132623b91131b0b89
bakNa2t <ok2.lock@gmail.com>
2025-11-02 21:00:46 +0300

    Create ProductFilters

---
+++ src/modules/products/ui/components/product-filters.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export const ProductFilters = () => {
+  return (
+    <div className="border rounded-md bg-white">
+      <div className="flex items-center justify-between p-4 border-b">
+        <p className="font-medium">Filters</p>
+
+        <button className="underline" onClick={() => {}} type="button">
+          Clear
+        </button>
+      </div>
+    </div>
+  );
+};
