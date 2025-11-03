ea307a5e0fb06c699e7ccc82e97e5eba437991f0
bakNa2t <ok2.lock@gmail.com>
2025-11-03 08:47:22 +0300

    Add ProductFilter comp with title

--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -1,5 +1,35 @@
 "use client";
 
+import { useState } from "react";
+import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+interface ProductFiltersProps {
+  title: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const ProductFilter = ({ title, className, children }: ProductFiltersProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const Icon = isOpen ? ChevronDownIcon : ChevronRightIcon;
+
+  return (
+    <div className={cn("flex flex-col gap-2  p-4 border-b", className)}>
+      <div
+        onClick={() => setIsOpen((current) => !current)}
+        className="flex items-center justify-between cursor-pointer"
+      >
+        <p className="font=medium">{title}</p>
+        <Icon className="size-5" />
+      </div>
+      {isOpen && children}
+    </div>
+  );
+};
+
 export const ProductFilters = () => {
   return (
     <div className="border rounded-md bg-white">
@@ -10,6 +40,10 @@ export const ProductFilters = () => {
           Clear
         </button>
       </div>
+
+      <ProductFilter title="Price">
+        <p>Price filter</p>
+      </ProductFilter>
     </div>
   );
 };
