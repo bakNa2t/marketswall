216359bf7cba606944a41be8f9691814103ae05f
bakNa2t <ok2.lock@gmail.com>
2025-11-09 16:42:25 +0300

    Add tags params

--- src/modules/products/hooks/use-product-filters.ts
+++ src/modules/products/hooks/use-product-filters.ts
@@ -1,11 +1,14 @@
 import { useQueryStates } from "nuqs";
-import { createLoader, parseAsString } from "nuqs/server";
+import { createLoader, parseAsString, parseAsArrayOf } from "nuqs/server";
 
 export const params = {
   minPrice: parseAsString.withOptions({
     clearOnDefault: true,
   }),
   maxPrice: parseAsString.withOptions({
+    clearOnDefault: true,
+  }),
+  tags: parseAsArrayOf(parseAsString).withOptions({
     clearOnDefault: true,
   }),
 };
