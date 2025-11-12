83992d62652a9818086fcac8466fe80f2bc7c8c6
bakNa2t <ok2.lock@gmail.com>
2025-11-12 07:37:38 +0300

    Create nuqs server search params

---
+++ src/modules/products/search-params.ts
@@ -0,0 +1,15 @@
+import { createLoader, parseAsString, parseAsArrayOf } from "nuqs/server";
+
+const params = {
+  minPrice: parseAsString.withOptions({
+    clearOnDefault: true,
+  }),
+  maxPrice: parseAsString.withOptions({
+    clearOnDefault: true,
+  }),
+  tags: parseAsArrayOf(parseAsString).withOptions({
+    clearOnDefault: true,
+  }),
+};
+
+export const loadProductFilters = createLoader(params);
