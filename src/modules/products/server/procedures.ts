a4d4b10732d84d5ac01ab01419f24b19e6aeea16
bakNa2t <ok2.lock@gmail.com>
2025-11-21 07:07:25 +0300

    Add input's cursor and limit

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -4,11 +4,14 @@ import { Category, Media } from "@/payload-types";
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -96,6 +99,8 @@ export const productsRouter = createTRPCRouter({
         depth: 1, // populate "category" & "image"
         where,
         sort,
+        page: input.cursor,
+        limit: input.limit,
       });
 
       return {
