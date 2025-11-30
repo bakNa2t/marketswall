c25636b0391fbf2665fe702a2e9ac10639129451
bakNa2t <ok2.lock@gmail.com>
2025-11-30 09:18:13 +0300

    Create tenants Navbar

---
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <nav className="h-20 border-b font-medium bg-white">
+      <div className="flex justify-between items-center mx-auto h-full max-w-(--breakpoint-xl) px-4 lg:px-12">
+        <p className="text-xl">Tenant</p>
+      </div>
+    </nav>
+  );
+};
