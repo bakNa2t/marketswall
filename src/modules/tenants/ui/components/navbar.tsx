ca87b92ab66527917041eae5c59a53c751042418
bakNa2t <ok2.lock@gmail.com>
2025-12-03 08:15:36 +0300

    Add trpc useSuspensQuery to get data

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -1,8 +1,20 @@
-export const Navbar = () => {
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+interface NavbarProps {
+  slug: string;
+}
+
+export const Navbar = ({ slug }: NavbarProps) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.tenants.getOne.queryOptions({ slug }));
+
   return (
     <nav className="h-20 border-b font-medium bg-white">
       <div className="flex justify-between items-center mx-auto h-full max-w-(--breakpoint-xl) px-4 lg:px-12">
-        <p className="text-xl">Tenant</p>
+        <p className="text-xl">{data.name}</p>
       </div>
     </nav>
   );
