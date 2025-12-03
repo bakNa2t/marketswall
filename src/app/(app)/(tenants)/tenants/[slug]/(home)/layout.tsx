e924a4694b3868e05ed3e3ea8b05cef91ee5526a
bakNa2t <ok2.lock@gmail.com>
2025-12-03 08:14:21 +0300

    Add slug's queryClient to prefetch tenant

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -1,3 +1,6 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
 import { Footer } from "@/modules/tenants/ui/components/footer";
 import { Navbar } from "@/modules/tenants/ui/components/navbar";
 
@@ -6,10 +9,22 @@ interface LayoutProps {
   params: Promise<{ slug: string }>;
 }
 
-const Layout = ({ children, params }: LayoutProps) => {
+const Layout = async ({ children, params }: LayoutProps) => {
+  const { slug } = await params;
+
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(
+    trpc.tenants.getOne.queryOptions({
+      slug,
+    })
+  );
+
   return (
     <div className="flex flex-col min-h-screen bg-[#f4f4f0]">
-      <Navbar />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Navbar slug={slug} />
+      </HydrationBoundary>
       <div className="flex-1 ">
         <div className="max-w-(--breakpoint-xl) mx-auto">{children}</div>
       </div>
