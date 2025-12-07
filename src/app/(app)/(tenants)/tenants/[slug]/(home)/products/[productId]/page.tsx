de6c898bb0965788139a667a42500c12cfeb2a30
bakNa2t <ok2.lock@gmail.com>
2025-12-07 09:58:48 +0300

    Create ProductIdPage

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -0,0 +1,21 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+interface ProductIdPageProps {
+  params: Promise<{ productId: string; slug: string }>;
+}
+
+const ProductIdPage = async ({ params }: ProductIdPageProps) => {
+  const { productId, slug } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.products.getOne.queryOptions({ slug }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductView productId={productId} tenantSlug={slug} />
+    </HydrationBoundary>
+  );
+};
+
+export default ProductIdPage;
