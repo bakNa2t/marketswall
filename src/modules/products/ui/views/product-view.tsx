31b6a5ff67d0785d3d02d00a950ece08efa4c0ed
bakNa2t <ok2.lock@gmail.com>
2025-12-09 07:22:27 +0300

    Add data by using trpc's useSuspenseQuery

--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 interface ProductViewProps {
   productId: string;
@@ -6,14 +10,19 @@ interface ProductViewProps {
 }
 
 export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.products.getOne.queryOptions({ id: productId })
+  );
+
   return (
     <div className="pxx-4 lg:px-12 py-10">
       <div className="border rounded-sm bg-white overflow-hidden">
         <div className="relative aspect-[3.9] border-b">
           <Image
-            src="/placeholder.png"
             fill
-            alt="cover"
+            alt={data.name}
+            src={data.image?.url || "/placeholder.png"}
             className="object-cover"
           />
         </div>
