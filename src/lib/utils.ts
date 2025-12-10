39044d97b0ff1c3ded41c8ba5544de0b8088638c
bakNa2t <ok2.lock@gmail.com>
2025-12-10 07:27:36 +0300

    Add formatCurrency fn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 export function generateTenantURL(tenantSlugL: string) {
   return `/tenants/${tenantSlugL}`;
 }
+
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+  }).format(value);
+}
