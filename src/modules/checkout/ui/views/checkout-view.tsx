7ff0d2b725872149f13e35648f1de9ea0d55b2f1
bakNa2t <ok2.lock@gmail.com>
2025-12-23 07:18:00 +0300

    Add CheckoutItem witn attr

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -1,18 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 import { useTRPC } from "@/trpc/client";
 import { useCart } from "../../hooks/use-cart";
-import { useEffect } from "react";
+import { generateTenantURL } from "@/lib/utils";
 
 interface CheckoutViewProps {
   tenantSlug: string;
 }
 
 export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
-  const { productIds, clearAllCarts } = useCart(tenantSlug);
+  const { productIds, removeProduct, clearAllCarts } = useCart(tenantSlug);
 
   const trpc = useTRPC();
   const { data, error } = useQuery(
@@ -28,5 +29,30 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
     }
   }, [error, clearAllCarts]);
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="pt-4 lg:pt-16 px-4 lg:px-12">
+      <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 lg:gap-16">
+        <div className="lg:col-span-4">
+          <div className="border rounded-md overflow-hidden bg-white">
+            {data?.docs.map((product, index) => (
+              <CheckoutItem
+                key={product.id}
+                id={product.id}
+                isLast={index === data.docs.length - 1}
+                imageUrl={product.image?.url}
+                name={product.name}
+                productUrl={`${generateTenantURL(product.tenant.slug)}/products/${product.id}`}
+                tenantUrl={generateTenantURL(product.tenant.slug)}
+                tenantName={product.tenant.name}
+                price={product.price}
+                onRemove={() => removeProduct(product.id)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="lg:col-span-3">Checkout sidebar</div>
+      </div>
+    </div>
+  );
 };
