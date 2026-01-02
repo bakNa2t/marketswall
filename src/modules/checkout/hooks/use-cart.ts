0d44a99b82d561d2b5afb32fd9df9544bf9add6f
bakNa2t <ok2.lock@gmail.com>
2026-01-02 16:22:32 +0300

    Update cart's methods states

--- src/modules/checkout/hooks/use-cart.ts
+++ src/modules/checkout/hooks/use-cart.ts
@@ -1,23 +1,26 @@
+import { useCallback } from "react";
 import { useCartStore } from "../store/use-cart-store";
 
 export const useCart = (tenantSlug: string) => {
-  const {
-    getCartByTenant,
-    addProduct,
-    removeProduct,
-    clearCart,
-    clearAllCarts,
-  } = useCartStore();
+  const addProduct = useCartStore((state) => state.addProduct);
+  const removeProduct = useCartStore((state) => state.removeProduct);
+  const clearCart = useCartStore((state) => state.clearCart);
+  const clearAllCarts = useCartStore((state) => state.clearAllCarts);
 
-  const productIds = getCartByTenant(tenantSlug);
+  const productIds = useCartStore(
+    (state) => state.tenantCarts[tenantSlug]?.productIds || []
+  );
 
-  const toggleProduct = (productId: string) => {
-    if (productIds.includes(productId)) {
-      removeProduct(tenantSlug, productId);
-    } else {
-      addProduct(tenantSlug, productId);
-    }
-  };
+  const toggleProduct = useCallback(
+    (productId: string) => {
+      if (productIds.includes(productId)) {
+        removeProduct(tenantSlug, productId);
+      } else {
+        addProduct(tenantSlug, productId);
+      }
+    },
+    [addProduct, removeProduct, productIds, tenantSlug]
+  );
 
   const isProductInCart = (productId: string) => {
     return productIds.includes(productId);
