d2dd60a83ab62d0d925a583e14302d53ccafc0d3
bakNa2t <ok2.lock@gmail.com>
2026-01-05 12:35:20 +0300

    Add View in Library btn if isPurchased is true

--- src/modules/products/ui/components/cart-button.tsx
+++ src/modules/products/ui/components/cart-button.tsx
@@ -2,14 +2,34 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
 import { useCart } from "@/modules/checkout/hooks/use-cart";
+import Link from "next/link";
 
 interface CartButtonProps {
   tenantSlug: string;
   productId: string;
+  isPurchased?: boolean;
 }
 
-export const CartButton = ({ tenantSlug, productId }: CartButtonProps) => {
+export const CartButton = ({
+  tenantSlug,
+  productId,
+  isPurchased,
+}: CartButtonProps) => {
   const cart = useCart(tenantSlug);
+
+  if (isPurchased) {
+    return (
+      <Button
+        asChild
+        variant="elevated"
+        className="flex-1 font-medium bg-white"
+      >
+        <Link prefetch href={`/library/${productId}`}>
+          View in Library
+        </Link>
+      </Button>
+    );
+  }
 
   return (
     <Button
