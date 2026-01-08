d10daffeb703a042631c67714302591084add8a7
bakNa2t <ok2.lock@gmail.com>
2026-01-08 09:19:49 +0300

    Create library ProductList

---
+++ src/modules/library/ui/components/product-list.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { InboxIcon } from "lucide-react";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+
+import { Button } from "@/components/ui/button";
+import { ProductCard, ProductCardSkeleton } from "./product-card";
+
+import { DEFAULT_LIMIT } from "@/constants";
+
+export const ProductList = () => {
+  const trpc = useTRPC();
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
+    useSuspenseInfiniteQuery(
+      trpc.library.getMany.infiniteQueryOptions(
+        { limit: DEFAULT_LIMIT },
+        {
+          getNextPageParam: (lastPage) => {
+            return lastPage.docs.length > 0 ? lastPage.nextPage : undefined;
+          },
+        }
+      )
+    );
+
+  if (data.pages?.[0]?.docs.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-y-4 w-full p-8 border border-black border-dashed bg-white rounded-lg">
+        <InboxIcon />
+        <p className="text-lg font-medium">No products found</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        {data?.pages
+          .flatMap((page) => page.docs)
+          .map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              imageUrl={product.image?.url}
+              tenantSlug={product.tenant?.slug}
+              tenantImageUrl={product.tenant?.image?.url}
+              reviewRating={4}
+              reviewCount={5}
+            />
+          ))}
+      </div>
+
+      <div className="flex justify-between pt-8">
+        {hasNextPage && (
+          <Button
+            variant="elevated"
+            onClick={() => fetchNextPage}
+            disabled={isFetchingNextPage}
+            className="text-base font-medium disabled:opacity-50 bg-white"
+          >
+            Load more
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export const ProductListSkeleton = () => {
+  return (
+    <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {Array.from({ length: DEFAULT_LIMIT }).map((_, index) => (
+        <ProductCardSkeleton key={index} />
+      ))}
+    </div>
+  );
+};
