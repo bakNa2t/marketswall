e56ada07b01281095a0eb65bdab9595700f36cad
bakNa2t <ok2.lock@gmail.com>
2026-01-16 07:36:31 +0300

    Add ReviewSidebar with style

--- src/modules/library/ui/views/product-view.tsx
+++ src/modules/library/ui/views/product-view.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { ArrowLeftIcon } from "lucide-react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+import { ReviewSidebar } from "../components/review-sidebar";
+
 import { useTRPC } from "@/trpc/client";
 
 interface ProductViewProps {
@@ -31,7 +33,21 @@ export const ProductView = ({ productId }: ProductViewProps) => {
         </div>
       </header>
 
-      <section className="max-w-(--breakpoint-xl) mx-auto px-4 lg:px-12 py-10"></section>
+      <section className="max-w-(--breakpoint-xl) mx-auto px-4 lg:px-12 py-10">
+        <div className="grid grid-cols-1 lg:grid-cols-7 gap-1 lg:gap-16">
+          <div className="lg:col-span-2">
+            <div className="gap-4 p-4 bg-white rounded-md border">
+              <ReviewSidebar productId={productId} />
+            </div>
+          </div>
+
+          <div className="lg:col-span-5">
+            <p className="font-medium text-muted-foreground italic">
+              No special content
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
