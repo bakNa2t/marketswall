91b635e9c4ad6c9dba3d854b3e622a4ddc1279e2
bakNa2t <ok2.lock@gmail.com>
2026-01-24 09:30:42 +0300

    Add dataWithSummarizedReviews to library's getMany mthd

--- src/modules/library/server/procedures.ts
+++ src/modules/library/server/procedures.ts
@@ -11,7 +11,7 @@ export const libraryRouter = createTRPCRouter({
     .input(
       z.object({
         productId: z.string(),
-      })
+      }),
     )
     .query(async ({ ctx, input }) => {
       const ordersData = await ctx.db.find({
@@ -63,7 +63,7 @@ export const libraryRouter = createTRPCRouter({
       z.object({
         cursor: z.number().default(1),
         limit: z.number().default(DEFAULT_LIMIT),
-      })
+      }),
     )
     .query(async ({ ctx, input }) => {
       const ordersData = await ctx.db.find({
@@ -90,9 +90,35 @@ export const libraryRouter = createTRPCRouter({
         },
       });
 
+      const dataWithSummarizedReviews = await Promise.all(
+        productsData.docs.map(async (doc) => {
+          const reviewsData = await ctx.db.find({
+            collection: "reviews",
+            pagination: false,
+            where: {
+              product: {
+                equals: doc.id,
+              },
+            },
+          });
+
+          return {
+            ...doc,
+            reviewCount: reviewsData.totalDocs,
+            reviewRating:
+              reviewsData.docs.length === 0
+                ? 0
+                : reviewsData.docs.reduce(
+                    (acc, review) => acc + review.rating,
+                    0,
+                  ) / reviewsData.totalDocs,
+          };
+        }),
+      );
+
       return {
         ...productsData,
-        docs: productsData.docs.map((doc) => ({
+        docs: dataWithSummarizedReviews.map((doc) => ({
           ...doc,
           image: doc.image as Media | null,
           tenant: doc.tenant as Tenant & { image: Media | null },
