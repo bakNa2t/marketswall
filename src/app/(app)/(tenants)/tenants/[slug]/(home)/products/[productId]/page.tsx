6363928f49f424ebae59508dc8a5aee90cdfc115
bakNa2t <ok2.lock@gmail.com>
2026-02-06 07:23:19 +0300

    Add react Suspense with fallback skeleton

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import { ProductView } from "@/modules/products/ui/views/product-view";
+import {
+  ProductView,
+  ProductViewSkeleton,
+} from "@/modules/products/ui/views/product-view";
 
 interface ProductIdPageProps {
   params: Promise<{ productId: string; slug: string }>;
@@ -15,7 +19,9 @@ const ProductIdPage = async ({ params }: ProductIdPageProps) => {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProductView productId={productId} tenantSlug={slug} />
+      <Suspense fallback={<ProductViewSkeleton />}>
+        <ProductView productId={productId} tenantSlug={slug} />
+      </Suspense>
     </HydrationBoundary>
   );
 };
