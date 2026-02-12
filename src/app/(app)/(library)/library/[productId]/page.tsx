874c5978ccaa59dca6812b65fd6c045a1caca218
bakNa2t <ok2.lock@gmail.com>
2026-02-12 07:12:17 +0300

    Wrap library's ProductView by Suspense

--- src/app/(app)/(library)/library/[productId]/page.tsx
+++ src/app/(app)/(library)/library/[productId]/page.tsx
@@ -1,7 +1,9 @@
+import { Suspense } from "react";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { ProductView } from "@/modules/library/ui/views/product-view";
+import { ProductCardSkeleton } from "@/modules/library/ui/components/product-card";
 
 interface PageProps {
   params: Promise<{
@@ -17,18 +19,20 @@ const Page = async ({ params }: PageProps) => {
   void queryClient.prefetchQuery(
     trpc.library.getOne.queryOptions({
       productId,
-    })
+    }),
   );
 
   void queryClient.prefetchQuery(
     trpc.reviews.getOne.queryOptions({
       productId,
-    })
+    }),
   );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProductView productId={productId} />;
+      <Suspense fallback={<ProductCardSkeleton />}>
+        <ProductView productId={productId} />;
+      </Suspense>
     </HydrationBoundary>
   );
 };
