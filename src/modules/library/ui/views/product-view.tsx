928fb501585a1cabba0065f0e079ad8ee00482c8
bakNa2t <ok2.lock@gmail.com>
2026-02-12 07:09:31 +0300

    Add library's ProductViewSkeleton

--- src/modules/library/ui/views/product-view.tsx
+++ src/modules/library/ui/views/product-view.tsx
@@ -60,3 +60,16 @@ export const ProductView = ({ productId }: ProductViewProps) => {
     </div>
   );
 };
+
+export const ProductViewSkeleton = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <nav className="w-full border-b p-4 bg-[#f4f4f0]">
+        <div className="flex items-center gap-2">
+          <ArrowLeftIcon className="size-4" />
+          <span className="text font-medium">Back to library</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
