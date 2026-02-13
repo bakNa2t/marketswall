04dfc006c6311736f460805a215730a442571ec6
bakNa2t <ok2.lock@gmail.com>
2026-02-13 07:43:45 +0300

    Add env dev/prod domain for generateTenantURL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function generateTenantURL(tenantSlugL: string) {
-  return `/tenants/${tenantSlugL}`;
+export function generateTenantURL(tenantSlug: string) {
+  if (process.env.NODE_ENV === "development") {
+    return `/tenants/${tenantSlug}`;
+  }
+
+  const protocol = "https";
+  const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+
+  return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(value: number) {
